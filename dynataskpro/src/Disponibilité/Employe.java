11c30438a6b8f2a704fdd7f15a0dd99fd13f044d
FredLuc12 <ablefonlin.f@yahoo.com>
2024-03-07 15:31:07 +0100

    ok

--- dynataskpro/src/Disponibilité/Employe.java
+++
@@ -1,9 +0,0 @@
-public class Employe implements Availability{
-    
-    private Calendrier calendrier;
-
-    @Override
-    public void mettreAJourDisponibilite() {
-        // Logique de mise à jour de la disponibilité
-    }
-}
